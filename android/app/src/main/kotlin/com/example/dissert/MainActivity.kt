56b33b59fa23fc5aac2172bb2cc8a3782d2f00e4
Fikri <dikri280d@gmail.com>
2021-02-11 20:09:59 +0700

    git push -u origin main

---
+++ android/app/src/main/kotlin/com/example/dissert/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dissert
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data.dart
@@ -0,0 +1,28 @@
+import 'motor.dart';
+
+List<Motor> dataMotor = [
+  Motor(
+      name: "H2", image: "h2.jpg", description: "Faster", price: "1.0000.000"),
+  Motor(
+      name: "Caferacer",
+      image: "caferacer.jpg",
+      description: "Faster",
+      price: "1.0000.000"),
+  Motor(
+      name: "CBR1000rr",
+      image: "cbr1000rr.jpg",
+      description: "Faster",
+      price: "1.0000.000"),
+  Motor(
+      name: "MT-09",
+      image: "mt09.jpg",
+      description: "Faster",
+      price: "1.0000.000"),
+  Motor(
+      name: "MT-10",
+      image: "mt10.jpg",
+      description: "Faster",
+      price: "1.0000.000"),
+  Motor(
+      name: "R1", image: "r1.jpg", description: "Faster", price: "1.0000.000"),
+];
